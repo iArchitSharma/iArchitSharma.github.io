0d5932a51eea98c777f874ff0c7716fff11c2e84
Archit Sharma <archit8679@gmail.com>
2022-08-08 22:11:20 +0530

    try fix flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./index.html",
-  "./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js",
-  ],
+  content: ["./src/**/*.{html,js}",
+   "./node_modules/flowbite/**/*.js",
+   "./index.html",],
   theme: {
     extend: {
       colors: {
